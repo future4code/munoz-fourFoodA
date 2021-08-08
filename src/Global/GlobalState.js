6afb863bdc536119bfe6358cfab0dc55ea7f5628
Kelvin de Sá Ferreira <kelvin-sa@live.com>
2021-08-08 18:43:19 -0300

    Adição carrinho

--- src/Global/GlobalState.js
+++ src/Global/GlobalState.js
@@ -1,97 +1,20 @@
 import GlobalStateContext from "./GlobalStateContext";
-import { useCartState } from "../hooks/useCartState";
-import { getRestaurants } from "../services/getRestaurants";
-import { getActiveOrder } from "../services/getActiveOrder";
-import React, { useContext, useEffect, useState } from "react";
-import { getUserData } from "../services/user";
+import React, { useState } from "react";
 
 export const GlobalState = (props) => {
-  const [restaurants, setRestaurants] = useState([]);
-  const [carrinho, setCarrinho] = useState([])
-  const [data, setData] = useState([]);
-  const [user, setUser] = useState({});
+  const [restaurant, setRestaurant] = useState({});
+  const [cart, setCart] = useState([])
 
-  const [
-    cartState,
-    addItemCart,
-    removeItemCart,
-    infoRest,
-    setInfoRest,
-    clearCart,
-  ] = useCartState();
-
-
-    const cart = {
-      cartState,
-      addItemCart,
-      removeItemCart,
-      infoRest,
-      setInfoRest,
-      clearCart,
-    };
-
-    
-  const [orders, setOrders] = useState([]);
-  const [isUpdate, setIsUpdate] = useState(false);
-
-  useEffect(() => {
-    (async () => {
-      const res = await getRestaurants();
-
-      if (res.status) {
-        setRestaurants(res.restaurants);
-      } else {
-        console.log(res.message);
-      }
-      setIsUpdate(false);
-    })();
-  }, [isUpdate]);
-
-  useEffect(() => {
-    (async () => {
-      const res = await getActiveOrder();
-
-      res.status && setOrders(res.order);
-    })();
-  }, []);
-
-  const token = localStorage.getItem("token");
-
-  useEffect(() => {
-    if (token) {
-      getUserData(setUser);
-    }
-  }, []);
-  const states = {
-    carrinho,
-    data,
-    user,
-    restaurants,
-  };
-  const setters = {
-    setCarrinho,
-    setData,
-    setUser,
-    setRestaurants,
-  };
+  const global = {
+    cart,
+    setCart,
+    restaurant,
+    setRestaurant
+  }
 
   return (
-    <GlobalStateContext.Provider value={{ states, setters, cart }}>
+    <GlobalStateContext.Provider value={ global }>
       {props.children}
     </GlobalStateContext.Provider>
   );
-};
-
-export const useGlobal = () => {
-  return useContext(GlobalStateContext);
-};
-
-export const useGlobalStates = () => {
-  const { states } = useGlobal();
-  return states;
-};
-
-export const useGlobalSetters = () => {
-  const { setters } = useGlobal();
-  return setters;
 };
--- src/Pages/CartPage/CartPage.js
+++ src/Pages/CartPage/CartPage.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import ProductCard from '../../Components/RestaurantProducts/ProductCard';
+import { useGlobal } from '../../Global/GlobalStateContext';
+import useRequestData from '../../hooks/useRequestData';
+import { BASE_URL } from '../../Constants/Urls';
 
-function CartPage () {
+function CartPage() {
+  const { restaurant, cart } = useGlobal()
+  const address = useRequestData(`${BASE_URL}/profile/address`)
+
+  let valores = 0
+  for (let i = 0; i < cart.length; i++) {
+    valores = valores + (cart[i].price * cart[i].quantity)
+  }
+
   return (
     <div>
-      <h1>CartPage</h1>
+      <p>Endereço de entrega</p>
+      <p>{address && address.address.street}, {address && address.address.number} - {address && address.address.neighbourhood}</p>
+
+      {cart.length ?
+        <div>
+          <h4>{restaurant.name}</h4>
+          <p>{restaurant.address}</p>
+          <p>{restaurant.deliveryTime}</p>
+
+          {cart.map(product => {
+            return <ProductCard product={product} />
+          })}
+        </div>
+        : <p>Carrinho Vazio</p>}
+
+      <p>Frete R$ {cart.length ? restaurant.shipping : "00"},00</p>
+
+      <div>
+        <h3>SUBTOTAL</h3>
+        <p>R$ {valores},00</p>
+      </div>
+
+      <p>Forma de pagamento</p>
+
+      <input type={"radio"} name={"payment"}/>
+      <label>Dinheiro</label>
+      <input type={"radio"} name={"payment"}/>
+      <label>Cartão de crédito</label>
+
+      <button>Confirmar</button>
     </div>
   );
 }
